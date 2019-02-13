1ff63ec2a0f28e4758d1af6e1108b84c6b651719
Jeanca8 <39780458+Jeanca8@users.noreply.github.com>
2019-02-12 19:27:26 -0600

    Add files via upload

---
+++ api.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+
+var Empresa = [];
+var EmpresaTemplate = {
+    RTN:"",
+    Empresa:"",
+    Correo:"",
+    Rubro:"",
+    Direccion:"",
+    Telefono:""
+}
+
+
+
+module.exports = router;
